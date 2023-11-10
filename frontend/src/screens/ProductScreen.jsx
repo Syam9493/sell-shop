cd9d461f340404f864f83f4524c1a6044994c161
Syam9493 <syamdokuparthi171@gmail.com>
2023-11-10 23:07:32 +0530

    implemented messages, errors,cart calculations..

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,31 +1,29 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import {useParams} from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import {Row,Col,Image,ListGroup, Card, Button} from 'react-bootstrap';
 import Rating from '../Components/Rating';
-import axios from 'axios';
+import Loader from '../Components/Loader';
+import Message from '../Components/Message';
+import { useGetProductQuery } from '../slices/productsApiSlice';
 
 const ProductScreen = () => {
    
-    const [product,setProduct] = useState({});
-
-   const {id: productId} = useParams();
-   
-   useEffect(()=>{
-    const fetchProduct = async () => {
-        const {data} = await axios.get(`/api/products/${productId}`);
-        setProduct(data);
-    }
-
-    fetchProduct();
-    
-   },[productId]);
+    const {id: productId} = useParams();
+const {data: product,  isLoading, error} = useGetProductQuery(productId);
    
    
 
   return (
     <>
-   <Link className='btn btn-light my-3' to='/'>Go Back</Link>
+   
+    { isLoading ? (
+       <Loader />
+    ) : error ? (
+        <Message variant='danger'>{ error?.data?.message || error.error }</Message>
+    ) : (
+        <>
+        <Link className='btn btn-light my-3' to='/'>Go Back</Link>
    <Row>
     <Col md={5}>
     <Image src={product.image} alt={product.name} fluid/>
@@ -68,6 +66,9 @@ const ProductScreen = () => {
      </Card>
     </Col>
    </Row>
+   
+        </>
+    )}
     </>
   )
 }
